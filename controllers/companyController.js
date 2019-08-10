015153f22d9266105a910817a4115a90e0684e8a
unknown <mauro.hdz99@gmail.com>
2019-08-10 10:08:43 -0500

    Working on api routes

---
+++ controllers/companyController.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const axios = require("axios");
+const db = require("../models/companiesData");
+const apiToken = process.env.apiToken;
+
+module.exports = {
+    getCompanies:(req, res) => {
+
+        axios.get('https://cloud.iexapis.com/beta/ref-data/symbols?token=' + apiToken).then( response => {
+        const goodDataArr = response.data;
+
+    
+    const stockArr =[];
+ goodDataArr.forEach((obj) => {
+    const newObj= {};
+
+     const companyName = obj.name;
+     const tickerSymbol = obj.symbol;
+     newObj.name = companyName;
+     newObj.symbol = tickerSymbol;
+     stockArr.push(newObj)
+ })
+          return stockArr
+
+    }).then(data => 
+        res.json(data)
+        )
+        .catch(err => console.log(err))
+
+
+        
+    }
+};
---
+++ routes/api/company.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const axios = require("axios");
+const db = require("../../models/companiesData");
+const apiToken = process.env.apiToken;
+const router = require("express").Router();
+const companyController = require("../../controllers/companyController")
+
+router.route("/")
+
+
+// router.post("/", (req, res) => {
+//     companyController.saveAll();
+
+// })
+
+module.exports = router;
