eaa5ddfa8a822aab993ea5d5aed63ef758f8e351
unknown <mauro.hdz99@gmail.com>
2019-08-11 21:22:53 -0500

    added card component

---
+++ client/src/components/companyCard/card.js
@@ -0,0 +1,24 @@
+import React from "React";
+
+
+
+class Card extends React.Component {
+    consructor(props) {
+        super(props)
+
+        this.state = {
+            Company: props.Name,
+            Symbol: props.Symbol
+        };
+
+        render() {
+            return (
+                
+            )
+        };
+
+    }
+
+
+
+}
